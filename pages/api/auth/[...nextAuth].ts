ee8899217daf0a20a2dab52e183e79612be8c884
KenTandrian <kenricktan11@gmail.com>
2022-12-22 17:04:24 +0700

    Add nextAuth file

---
+++ pages/api/auth/[...nextAuth].ts
@@ -0,0 +1,18 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import FacebookProvider from "next-auth/providers/facebook";
+export const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID || "",
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET || "",
+    }),
+    // ...add more providers here
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/auth/signin",
+  },
+};
+
+export default NextAuth(authOptions);
