6e838bd4a7c59f2cf47d197b5c7bd63ae730768d
KenTandrian <kenricktan11@gmail.com>
2022-12-22 16:33:46 +0700

    Add loader component

--- app/MessageList.tsx
+++ app/MessageList.tsx
@@ -6,7 +6,11 @@ import { Message } from "../typings";
 import fetcher from "../utils/fetchMessages";
 import MessageComponent from "./MessageComponent";
 
-const MessageList = () => {
+type Props = {
+  initialMessages: Message[];
+};
+
+const MessageList = ({ initialMessages }: Props) => {
   const {
     data: messages,
     error,
@@ -36,7 +40,7 @@ const MessageList = () => {
 
   return (
     <div className="space-y-5 px-5 pt-8 pb-32 max-w-2xl xl:max-w-4xl mx-auto">
-      {messages?.map((message) => (
+      {(messages || initialMessages).map((message) => (
         <MessageComponent key={message.id} message={message} />
       ))}
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { Message } from "../typings";
 import ChatInput from "./ChatInput";
 import MessageList from "./MessageList";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const data = await fetch(`${process.env.VERCEL_URL}/api/getMessages`).then(
+    (res) => res.json()
+  );
+
+  const messages: Message[] = data.messages;
   return (
     <main>
-      <MessageList />
+      <MessageList initialMessages={messages} />
       <ChatInput />
     </main>
   );
