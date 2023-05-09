4bec1c74d5bf98ed7711074cc002757a39dbecf2
KenTandrian <kenricktan11@gmail.com>
2023-05-09 12:26:21 +0700

    fix: middleware not working

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,7 +3,17 @@ import React from "react";
 import { SessionProvider } from "next-auth/react";
 
 const Providers = ({ session, children }: any) => {
-  return <SessionProvider session={session}>{children}</SessionProvider>;
+  return (
+    <SessionProvider session={session}>
+      {session ? (
+        children
+      ) : (
+        <p className="text-center m-10">
+          You need to be signed up to use the app.
+        </p>
+      )}
+    </SessionProvider>
+  );
 };
 
 export default Providers;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,17 @@
-export { default } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  if (request.cookies.get("next-auth.session-token")) {
+    return NextResponse.next();
+  } else {
+    return NextResponse.redirect(
+      new URL("/auth/signin", request.nextUrl.origin)
+    );
+  }
+}
 
 // Secures the matching routes...
 export const config = {
-  matcher: ["/"],
+  matcher: "/",
 };
