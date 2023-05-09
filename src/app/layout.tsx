9cadf71f68341377a9ae849c5295decbbec4a272
KenTandrian <kenricktan11@gmail.com>
2023-05-09 12:28:49 +0700

    fix(meta): deprecate head component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import "src/styles/globals.scss";
 import Header from "src/components/Header";
 import { getServerSession } from "next-auth";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Meta Messenger",
+  description: "A messenger app built with Next.js and NextAuth.js",
+  icons: "/favicon.ico",
+};
 
 type Props = {
   children: React.ReactNode;
