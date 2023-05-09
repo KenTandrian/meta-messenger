51dcfbde36b3b5b2e3224f03a92d839de487a0bc
KenTandrian <kenricktan11@gmail.com>
2023-05-09 12:44:09 +0700

    fix(auth): change cookie name

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,10 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  if (request.cookies.get("next-auth.session-token")) {
+  if (
+    request.cookies.get("next-auth.session-token") ||
+    request.cookies.get("__Secure-next-auth.session-token")
+  ) {
     return NextResponse.next();
   } else {
     return NextResponse.redirect(
